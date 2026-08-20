8b6ddf93905f62eb3b8293503b0a2da41d7637e2
andreyribeiro392-coder <andreyribeiro392@gmail.com>
2026-08-20 13:10:57 -0300

    Importa ajustes de privacidade visual

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./directory.css";
 import "./client-simple.css";
 import "./enhancements.css";
 import "./security.css";
+import "./privacy-fixes.css";
 
 export const metadata: Metadata = {
   title: "Fila Fácil",
